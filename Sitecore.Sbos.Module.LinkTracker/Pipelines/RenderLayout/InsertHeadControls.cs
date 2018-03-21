87db6599bcfac1a95068c3e8acd50a336e78c2f2
Koen Heye <koen.heye@gmail.com>
2018-03-21 14:53:47 +0100

    Added support for WebForms. Javascript gets added to the head with a custom pipeline. Insertion of the jQuery reference can now be enabled with a parameter in config file. Its not automatic anymore.

--- Sitecore.Sbos.Module.LinkTracker/Pipelines/RenderLayout/InsertHeadControls.cs
+++ Sitecore.Sbos.Module.LinkTracker/Pipelines/RenderLayout/InsertHeadControls.cs
@@ -4,6 +4,8 @@ namespace Sitecore.Sbos.Module.LinkTracker.Pipelines.RenderLayout
 {
     public class InsertHeadControls : RenderRenderingProcessor
     {
+        public bool RenderJQuery { get; set; }
+
         public override void Process(RenderRenderingArgs args)
         {
             if (Context.Site.Name == "shell")
@@ -15,7 +17,9 @@ namespace Sitecore.Sbos.Module.LinkTracker.Pipelines.RenderLayout
             {
                 if (args.Writer.ToString().Contains("<title>"))
                 {
-                    args.Writer.WriteLine(Data.Constants.LinkTrackerConstants.JQueryScript);
+                    if(RenderJQuery)
+                        args.Writer.WriteLine(Data.Constants.LinkTrackerConstants.JQueryScript);
+
                     args.Writer.WriteLine(Data.Constants.LinkTrackerConstants.LinkTrackerMgrScript);
                 }
             }
---
+++ Sitecore.Sbos.Module.LinkTracker/Pipelines/RenderLayout/InsertHeadControlsWebForms.cs
@@ -0,0 +1,32 @@
+﻿using Sitecore.Pipelines.RenderLayout;
+using Sitecore.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+
+namespace Sitecore.Sbos.Module.LinkTracker.Pipelines.RenderLayout
+{
+    public class InsertHeadControlsWebForms
+    {
+        public bool RenderJQuery { get; set; }
+
+        public void Process(RenderLayoutArgs args)
+        {
+            // no need to run our code in the shell
+            if (Sitecore.Context.Site.Name == "shell")
+                return;
+
+            Control head = WebUtil.FindControlOfType(Sitecore.Context.Page.Page, typeof(HtmlHead));
+            if (head != null)
+            {
+                if(RenderJQuery)
+                    head.Controls.Add(new LiteralControl(Data.Constants.LinkTrackerConstants.JQueryScript));
+
+                head.Controls.Add(new LiteralControl(Data.Constants.LinkTrackerConstants.LinkTrackerMgrScript));
+            }
+        }
+    }
+}
